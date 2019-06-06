798aefcfe0e9763589a7e0757fe6f9c1d66fa243
Lev Tikhonov <ltikhonov@tv-culture.ru>
2019-06-06 11:14:02 +0300

    HW2 - rev 2 corrected after code review 2

--- src/MainArray.java
+++ src/MainArray.java
@@ -13,7 +13,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save uuid | update uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -35,6 +35,12 @@ public class MainArray {
                     r.uuid = uuid;
                     ARRAY_STORAGE.save(r);
                     printAll();
+                    break;
+                case "update":
+                    Resume rOld = ARRAY_STORAGE.get(uuid);
+                    Resume rNew = new Resume();
+                    rNew.uuid = rOld.uuid;
+                    ARRAY_STORAGE.update(rNew);
                     break;
                 case "delete":
                     ARRAY_STORAGE.delete(uuid);
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -3,6 +3,7 @@
  */
 public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static int ARRAY_STORAGE_LIMIT = 10_000;
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -14,17 +15,14 @@ public class MainTestArrayStorage {
         Resume r2new = new Resume();
         r2new.uuid = "uuid2";
 
-
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
-
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
-
 
         ARRAY_STORAGE.save(r2new);
 
@@ -34,9 +32,6 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.get("uuid111");
         ARRAY_STORAGE.delete("uuid111");
 
-
-
-
         printAll();
         ARRAY_STORAGE.delete(r1.uuid);
         printAll();
@@ -45,15 +40,13 @@ public class MainTestArrayStorage {
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        {
-            for (int i=1; i<=10001;i++){
-                Resume r = new Resume();
-                r.uuid = "uuid" + i;
-                ARRAY_STORAGE.save(r);
-            }
+        for (int i=0; i<=ARRAY_STORAGE_LIMIT; i++){
+            Resume r = new Resume();
+            r.uuid = "uuid" + i;
+            ARRAY_STORAGE.save(r);
         }
-        System.out.println("Size: " + ARRAY_STORAGE.size());
 
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
