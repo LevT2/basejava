96067eb04a1cce70f47f7cd4fc7d8de99f5f239d
Lev Tikhonov <ltikhonov@tv-culture.ru>
2019-06-20 00:02:39 +0300

    initial for HW3

---
+++ src/main/java/ru/javawebinar/basejava/storage/impl/SortedArrayStorage.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.basejava.storage.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.AbstractArrayStorage;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+    protected final Logger log = LoggerFactory.getLogger(SortedArrayStorage.class);
+
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+}
