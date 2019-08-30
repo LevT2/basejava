dd8943ecac5d6e11db9ef2429ce333c1dd751e6a
Lev Tikhonov <levt2@mail.ua>
2019-08-30 11:35:43 +0300

    fix resume is not pushed

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -19,6 +19,7 @@ public class Resume implements Comparable<Resume> {
         this(UUID.randomUUID().toString(), fullName);
     }
 
+
     public Resume(String uuid, String fullName) {
         this.uuid = uuid;
         this.fullName = fullName;
